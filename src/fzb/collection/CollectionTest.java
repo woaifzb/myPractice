38c4608dd1026e4b9336b28e4b5beb9f7a213037
woaifzb <fzb_xs@sina.com>
2019-06-24 22:16:55 +0800

    好久没提交了

---
+++ src/fzb/collection/CollectionTest.java
@@ -0,0 +1,11 @@
+package fzb.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CollectionTest {
+	public static void main(String[] args) {
+		Map map=new HashMap();
+		
+	}
+}
